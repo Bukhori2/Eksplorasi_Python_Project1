d5cfd4af1c98fb718cd9c09c47517abfb9d69553
nazwarizqikatif425-lang <nazwa.rizqika.tif425@polban.ac.id>
2026-02-24 22:16:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,3 @@ def main():
     print("Hello, World!")
 if __name__ == "__main__":
     main()
-    
-#modul notification
-from datetime import datetime
-from modules import task_manager
-
-def check_deadlines():
-    today = datetime.today().strftime("%Y-%m-%d")
-    for task in task_manager.tasks:
-        if not task["done"] and task["deadline"] == today:
-            print(f"⚠️ Reminder: Deadline tugas '{task['title']}' hari ini!")
